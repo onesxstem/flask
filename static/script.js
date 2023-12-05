7eb2292fcdeba7791c183abc68d6c0613bc1241e
onesxstem <152931154+onesxstem@users.noreply.github.com>
2023-12-04 22:33:48 -0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,8 +2,20 @@ function calculateAdjustedIncome() {
     var income = parseFloat(document.getElementById('income').value);
 
     if (!isNaN(income)) {
-        // Assuming the Flask route for calculation is '/calculate'
-        window.location.href = '/calculate?income=' + income;
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/form', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                // Handle the response, if needed
+                console.log(xhr.responseText);
+            }
+        };
+
+        // Encode the data and send the request
+        var formData = 'income=' + encodeURIComponent(income);
+        xhr.send(formData);
     } else {
         document.getElementById('error').innerText = 'Invalid input. Please enter a valid numeric value.';
     }
