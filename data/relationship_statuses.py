f59b9af56789b9c7d05e78006ce1953e6b2fa57b
onesxstem <152931154+onesxstem@users.noreply.github.com>
2024-01-03 14:08:43 -0500

    Add files via upload

---
+++ data/relationship_statuses.py
@@ -0,0 +1,7 @@
+relationship_statuses = [
+    'Single',
+    'Married Filing Jointly',
+    'Married Filing Separately',
+    'Head of Household',
+    'Qualifying Widow(er) with Dependent Child',
+]
