bbc7cd08084db88ecebca5d8bd9e7e0b05a106c7
onesxstem <152931154+onesxstem@users.noreply.github.com>
2024-01-03 18:32:11 -0500

    Delete data directory

--- data/relationship_statuses.py
+++
@@ -1,7 +0,0 @@
-relationship_statuses = [
-    'Single',
-    'Married Filing Jointly',
-    'Married Filing Separately',
-    'Head of Household',
-    'Qualifying Widow(er) with Dependent Child',
-]
